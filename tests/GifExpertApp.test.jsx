1ba87cefc1f44ae9af6fa8f7d31a51ade8934f41
Sebastian <sebastiancastillo1402@gmail.com>
2023-02-02 12:24:02 -0500

    finished test in GifExpertApp

--- tests/GifExpertApp.test.jsx
+++ tests/GifExpertApp.test.jsx
@@ -1,3 +1,21 @@
+const { render, screen, fireEvent } = require("@testing-library/react");
+const { GifExpertApp } = require("../src/GifExpertApp");
+
 describe("Pruebas en GifExpertApp", () => {
-  test("", () => {});
+  test("Debe de agregar una categoria cuando envio un input nuevo", () => {
+    render(<GifExpertApp />);
+    const input = screen.getByRole("textbox");
+    const form = screen.getByRole("form");
+    fireEvent.input(input, { target: { value: 'Nuevo Gif' } });
+    fireEvent.submit(form);
+    expect(screen.getByText('Nuevo Gif')).toBeTruthy();
+  });
+  test("Si la categoria ya esta no debera de agregarse", ()=>{
+    render(<GifExpertApp />);
+    const input = screen.getByRole("textbox");
+    const form = screen.getByRole("form");
+    fireEvent.input(input, { target: { value: 'Demon Slayer' } });
+    fireEvent.submit(form);
+    expect(screen.getAllByText('Demon Slayer').length).toBe(1);
+  })
 });
--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -1,4 +1,4 @@
-import { fireEvent, getByRole, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { AddCategory } from "../../src/components/AddCategory";
 
 describe("Pruebas en componente AddCategory", () => {
